c6e075111d5500b043a11131bcaafa6d964de697
Quinten Steenhuis <qsteenhuis@gmail.com>
2021-12-08 20:22:07 -0500

    Add in stats

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.MTVHousing',
-      version='1.0.1',
+      version='1.0.2',
       description=('A docassemble extension.'),
       long_description='# docassemble.MotiontovacateDefaultJudgment\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nMichael Cronin, mcronin@su.suffolk.edu\r\n\r\n',
       long_description_content_type='text/markdown',
